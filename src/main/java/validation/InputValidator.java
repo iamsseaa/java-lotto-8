48acd9e31741717937c3d0df7d80157b195d6435
iamsseaa <homingsea@gmail.com>
2025-11-03 20:09:41 +0900

    feat: 로또 구입 최소 금액 미만인 경우 예외 처리

--- src/main/java/validation/InputValidator.java
+++ src/main/java/validation/InputValidator.java
@@ -8,12 +8,19 @@ public class InputValidator {
 
     public static int lottoPrice(int lottoPrice) {
         validateAmountByThousand(lottoPrice);
+        validateMinimumPrice(lottoPrice);
         return lottoPrice;
     }
 
     private static void validateAmountByThousand(int lottoPrice) {
         if (lottoPrice % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 로또 구입 금앱은 1000원 단위로 입력 가능합니다.");
+        }
+    }
+
+    private static void validateMinimumPrice(int lottoPrice) {
+        if (lottoPrice < 1000) {
+            throw new IllegalArgumentException("[Error] 로또 구입 금액은 1000원 이상부터 가능합니다.");
         }
     }
 }
