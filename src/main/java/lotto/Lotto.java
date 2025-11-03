3f8cb5258e2572183ff388c644ef7a987344cc43
iamsseaa <homingsea@gmail.com>
2025-11-03 19:48:35 +0900

    feat: 당첨 로또 번호 입력이  중복된 경우 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,15 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +19,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> numbersForCheck = new HashSet<>(numbers);
+
+        if (numbers.size() != numbersForCheck.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
 }
